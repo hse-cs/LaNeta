bd9bb2ab96ccbb8a95f7d50b300cdc87c852f18b
AnstMikh <62886473+AnstMikh@users.noreply.github.com>
2021-09-29 20:45:38 +0300

    Update preparation.sh

--- utilites/preparation.sh
+++ utilites/preparation.sh
@@ -6,7 +6,7 @@ read -p "Inser name of ONE population to be transformed to morgans " answer
 for i in (echo ${answer} | tr " " "\n")
   do
     plink --vcf ../${i}.vcf.gz --chr 1-22 --snps-only --make-bed --out ${i}
-    plink --bfile ${i} --recode vcf --out nomaf${i}.${i}
+    plink --bfile ${i} --recode vcf --out ${i}.${i}
     bcftools query -f '%CHROM\t%POS\t%ID\n' ${i}.${i}.vcf >> gen.${i}.txt
     for j in {1..22}; do grep -w ${j} gen.${i}.txt >> gen.${i}.chr${j}.txt; done
     srun python phys2gen.py ${i}
